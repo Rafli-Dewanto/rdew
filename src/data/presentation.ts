3855467c9bbf750a4b81d31a97d502ff7b030167
Rafli Dewanto <raflidewanto97@gmail.com>
2023-12-05 14:11:21 +0700

    update description

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,36 +1,37 @@
 type Social = {
-  label: string;
-  link: string;
+    label: string;
+    link: string;
 };
 
 type Presentation = {
-  mail: string;
-  title: string;
-  description: string;
-  socials: Social[];
-  profile?: string;
+    mail: string;
+    title: string;
+    description: string;
+    socials: Social[];
+    profile?: string;
 };
 
 const presentation: Presentation = {
-  mail: "dewantorafli@gmail.com",
-  title: "Hi, I’m Rafli Dewanto 👋",
-  description:
-    "A *Software Engineer* based in Bekasi, Indonesia. I'm currently learning *Fullstack Web Development* with *Next.js* and *Back-End Development* with *Go*. I also pursuing a *Computer Science* degree at *Gunadarma* University",
-  socials: [
-    {
-      label: "Instagram",
-      link: "https://www.instagram.com/rafli.dewanto/",
-    },
-    {
-      label: "LinkedIn",
-      link: "https://www.linkedin.com/in/rd09/",
-    },
-    {
-      label: "Github",
-      link: "https://github.com/Rafli-Dewanto",
-    },
-  ],
-  profile: 'https://media.licdn.com/dms/image/D5603AQEljt1TTN_XTA/profile-displayphoto-shrink_400_400/0/1698132288654?e=1704931200&v=beta&t=FHmjPvwdpQfulVO6MZ0J-HhfLrzVslLZWVPCXxnNEY0'
+    mail: "dewantorafli@gmail.com",
+    title: "Hi, I’m Rafli Dewanto 👋",
+    description:
+        "I am a *Software Engineer* from *Indonesia*, currently working with *Next.js* for *Fullstack Web Development*. In my leisure time, I am learning *Back-End Development* with *Go* and writing articles about my learning.",
+    socials: [
+        {
+            label: "Instagram",
+            link: "https://www.instagram.com/rafli.dewanto/",
+        },
+        {
+            label: "LinkedIn",
+            link: "https://www.linkedin.com/in/rd09/",
+        },
+        {
+            label: "Github",
+            link: "https://github.com/Rafli-Dewanto",
+        },
+    ],
+    profile:
+        "https://media.licdn.com/dms/image/D5603AQEljt1TTN_XTA/profile-displayphoto-shrink_400_400/0/1698132288654?e=1704931200&v=beta&t=FHmjPvwdpQfulVO6MZ0J-HhfLrzVslLZWVPCXxnNEY0",
 } as const;
 
 export default presentation;
